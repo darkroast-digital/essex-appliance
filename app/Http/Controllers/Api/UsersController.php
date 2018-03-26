71c4644f7fd7955f8b9759d2654c4278b1882441
Josh Stobbs <joshstobbs@gmail.com>
2018-03-26 10:38:45 -0400

    added users api

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Spatie\Fractal\Fractal;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Transformers\UserTransformer;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = User::paginate(10);
+        $users = $paginator->getCollection();
+
+        $data = Fractal::create()
+                ->collection($users, new UserTransformer())
+                ->paginateWith(new IlluminatePaginatorAdapter($paginator))
+                ->toArray();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
