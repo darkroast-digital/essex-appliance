69c18913962fb096ca5845bcb65b99a1f9adad81
Josh Stobbs <joshstobbs@gmail.com>
2018-03-26 13:55:52 -0400

    added products index

---
+++ resources/views/panel/products/index.blade.php
@@ -0,0 +1,11 @@
+@extends('panel.layouts.master')
+
+@section('content')
+
+    <div class="container">
+        <div class="column">
+            Product Index
+        </div>
+    </div>
+
+@endsection
