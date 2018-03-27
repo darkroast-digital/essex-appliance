3a02d7f9bddbab6286a133de08271397d30e577d
Josh Stobbs <joshstobbs@gmail.com>
2018-03-27 14:17:17 -0400

    added more to product form

--- resources/views/panel/layouts/partials/_nav.blade.php
+++ resources/views/panel/layouts/partials/_nav.blade.php
@@ -8,7 +8,7 @@
     
             <ul class="nav-menu">
                 <li class="dropdown">
-                    <a href="{{ route('panel.dashboard') }}">
+                    <a href="{{ route('panel.dashboard') }}" class="{{ strpos(app('request')->url(), 'dashboard') ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 20 20">
                             <path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z" class="icon" />
                         </svg>
@@ -21,7 +21,7 @@
                 </li>
 
                 <li class="dropdown">
-                    <a href="{{ route('panel.products.index') }}">
+                    <a href="{{ route('panel.products.index') }}" class="{{ strpos(app('request')->url(), 'product') ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 20 20">
                             <path d="M0 10V2l2-2h8l10 10-10 10L0 10zm4.5-4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" class="icon" />
                         </svg>
@@ -34,7 +34,7 @@
                 </li>
 
                 <li class="dropdown">
-                    <a href="#0">
+                    <a href="#0" class="{{ strpos(app('request')->url(), 'user') ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 20 20">
                             <path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z" class="icon" />
                         </svg>
--- resources/views/panel/products/partials/_form.blade.php
+++ resources/views/panel/products/partials/_form.blade.php
@@ -3,14 +3,80 @@
     <div class="container">
 
         <div class="column-3">
-            Image
+            <div class="block --has-margin-bottom">
+                Image
+            </div>
+
+            <!-- /.block -->
+
+            <div class="block --has-margin-bottom">
+                tags <br>
+                featured
+            </div>
+
+            <!-- /.block -->
+
+            <div class="block --has-margin-bottom">
+                colours
+            </div>
+
+            <!-- /.block -->
+
         </div>
+
+        <!-- /.column-3 -->
 
         <div class="column-9">
-            {{--  <h1 class="page-title">{{ $mode === 'create' ? 'Add Product' : 'Edit Product' }}</h1>  --}}
+            <div class="block --has-margin-bottom">
+                <h6>{{ $mode === 'create' ? 'Add' : 'Edit' }} Product</h6>
+                <hr>
+                <div class="field">
+                    <label for="name">Product Name</label>
+                    <input type="text" name="name" placeholder="Add product name..." value="{{ isset($product->name) ? $product->name : '' }}">
+                </div>
+
+                <!-- /.field -->
+
+                <div class="form-row">
+                    <div class="field">
+                        <label for="sku">Product SKU</label>
+                        <input type="text" name="sku" placeholder="Add product SKU..." value="{{ isset($product->sku) ? $product->sku : '' }}">
+                    </div>
+
+                    <!-- /.field -->
+
+                    <div class="field">
+                        <label for="upc">Product UPC</label>
+                        <input type="text" name="upc" placeholder="Add product UPC..." value="{{ isset($product->upc) ? $product->upc : '' }}">
+                    </div>
+
+                    <!-- /.field -->
+                </div>
+
+                <!-- /.form-row -->
+
+            </div>
+
+            <!-- /.block -->
+
+            <div class="block --has-margin-bottom">
+
+                <div class="field">
+                    <label for="description">Product Description</label>
+                    <textarea name="description" placeholder="Add product description...">{{ isset($product->description) ? $product->description : '' }}</textarea>
+                </div>
+
+            </div>
+
+            <!-- /.block -->
+
         </div>
 
+        <!-- /.column-9 -->
+
     </div>
+
+    <!-- /.container -->
 
 </form>
 
